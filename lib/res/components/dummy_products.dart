772d1dcc6bf799bf17f234b7baaaaa6f4e7a9620
Z-i-n-k-s <zishancodes1@gmail.com>
2023-12-25 23:42:34 +0600

    added update product list

--- lib/res/components/dummy_products.dart
+++ lib/res/components/dummy_products.dart
@@ -6,27 +6,27 @@ class DummyFoodProducts {
     ProductModel(
       productName: "Pizza",
       productImage: AppAssets.foodPizza,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Burger",
       productImage: AppAssets.foodBurger,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Steak",
       productImage: AppAssets.foodsteak,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Briyani",
       productImage: AppAssets.foodbiriyani,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Kebab",
       productImage: AppAssets.foodkebab,
-      price: '\$200',
+      price: '200',
     ),
   ];
 }
@@ -36,22 +36,22 @@ class DummyPoloProducts {
     ProductModel(
       productName: "Black Polo T-shirt",
       productImage: AppAssets.poloBlack,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Blue Polo T-shirt",
       productImage: AppAssets.poloBlue,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Navy Blue Polo T-shirt",
       productImage: AppAssets.poloNavyBlue,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "White Polo T-shirt",
       productImage: AppAssets.poloWhite,
-      price: '\$200',
+      price: '200',
     ),
   ];
 }
@@ -61,17 +61,17 @@ class DummyTshirtProducts {
     ProductModel(
       productName: "Black T-Shirt",
       productImage: AppAssets.tBlack,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "White T-Shirt",
       productImage: AppAssets.tWhite,
-      price: '\$200',
+      price: '200',
     ),
     ProductModel(
       productName: "Grey T-Shirt",
       productImage: AppAssets.tGrey,
-      price: '\$200',
+      price: '200',
     ),
   ];
 }
