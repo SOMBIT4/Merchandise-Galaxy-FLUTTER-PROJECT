f8ec3fe6d6540f8d91b12de300962e92cbf19978
SOMBIT MAZUMDER <115360765+SOMBIT4@users.noreply.github.com>
2023-12-27 09:43:51 +0600

    added splash screens

--- lib/pages/splassh_screen.dart
+++ lib/pages/splassh_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:merchendise_galaxy/pages/login_pages.dart';
+import 'package:merchendise_galaxy/res/app_assets/app_assets.dart';
+import 'package:merchendise_galaxy/res/colors/app_color.dart';
 
 class splashscreen extends StatefulWidget {
   const splashscreen({super.key});
@@ -33,31 +35,31 @@ class _splashscreenState extends State<splashscreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey.shade200,
+      backgroundColor: AppColor.backGroundColor,
       body: Container(
         padding: const EdgeInsets.fromLTRB(10, 50, 20, 20),
         child: Column(
           children: [
             Image.asset(
               'lib/images/logo1.png',
-              height: 400,
-              width: 400,
+              height: 350,
+              width: 350,
             ),
             //text
             Container(
-              padding: const EdgeInsets.only(left: 35),
+              padding: const EdgeInsets.only(left: 5, top: 40),
               child: Text(
                 'Merchandise',
                 style: TextStyle(
                     color: Colors.blue,
                     fontFamily: 'Fontmain',
-                    fontSize: 40,
+                    fontSize: 35,
                     fontWeight: FontWeight.bold),
               ),
             ),
             //text
             Container(
-              padding: const EdgeInsets.only(left: 120),
+              padding: const EdgeInsets.only(left: 140, top: 10),
               child: Text(
                 'Galaxy',
                 style: TextStyle(
