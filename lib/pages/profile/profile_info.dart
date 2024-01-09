6d26d68c66762b92148b037eb96df175e4c28cd2
SOMBIT MAZUMDER <115360765+SOMBIT4@users.noreply.github.com>
2024-01-10 03:29:47 +0600

    read 50%code written but doesnt work

--- lib/pages/profile/profile_info.dart
+++ lib/pages/profile/profile_info.dart
@@ -1,14 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 import 'package:merchendise_galaxy/res/app_assets/app_assets.dart';
 
-class ProfileInfo extends StatelessWidget {
+class ProfileInfo extends StatefulWidget {
   const ProfileInfo({super.key});
+
+  @override
+  State<ProfileInfo> createState() => _ProfileInfoState();
+}
+
+class _ProfileInfoState extends State<ProfileInfo> {
+  final user = FirebaseAuth.instance.currentUser!;
+  //docid
+  List<String> docids = [];
+//get doc id
+  Future getdocid() async {
+    await FirebaseFirestore.instance
+        .collection('user')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((element) {
+              print(element.reference);
+              docids.add(element.reference.id);
+            }));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blue,
         leading: IconButton(
             onPressed: () => Navigator.pop(context),
             icon: const Icon(LineAwesomeIcons.angle_left)),
@@ -22,33 +44,49 @@ class ProfileInfo extends StatelessWidget {
         child: Container(
           padding: const EdgeInsets.all(60),
           child: Container(
-            height: 400,
-            width: 550,
+            height: 500,
+            width: 400,
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              //  mainAxisAlignment: MainAxisAlignment.center,
+              //crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 // -- IMAGE with ICON
-                Stack(
-                  children: [
-                    SizedBox(
-                      width: 180,
-                      height: 180,
+                //  Stack(
+                //  children: [
+                /*   SizedBox(
+                      width: 150,
+                      height: 150,
                       child: ClipRRect(
                         borderRadius: BorderRadius.circular(100),
                         child: Image.asset(AppAssets.profileImg),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 40),
+                    ),*/
+                Expanded(
+                    child: FutureBuilder(
+                  future: getdocid(),
+                  builder: (context, snapshot) {
+                    return ListView.builder(
+                        itemCount: docids.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(
+                              docids[index],
+                              style: TextStyle(fontSize: 60),
+                            ),
+                          );
+                        });
+                  },
+                ))
+              ],
+            ),
+            /* const SizedBox(height: 40),
                 Text('Name', style: Theme.of(context).textTheme.headline4),
                 const SizedBox(height: 60),
                 Text('Email', style: Theme.of(context).textTheme.bodyText2),
-                const SizedBox(height: 40),
+                const SizedBox(height: 40),*/
 
-                // -- Form Fields
-                /* Form(
+            // -- Form Fields
+            /* Form(
                   child: Column(
                     children: [
                       const SizedBox(height: 20),
@@ -61,11 +99,11 @@ class ProfileInfo extends StatelessWidget {
                     ],
                   ),
                 ),*/
-              ],
-            ),
+            // ],
           ),
         ),
       ),
+      //    ),
     );
   }
 }
